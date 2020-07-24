d347d47d5e8728a6441db00712526fd0fff32c70
18055857 <18055857@myrp.edu.sg>
2020-07-24 12:38:55 +0800

    updated

--- app/src/main/java/sg/edu/rp/demoswiper/Frag1.java
+++ app/src/main/java/sg/edu/rp/demoswiper/Frag1.java
@@ -7,12 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Frag1 extends Fragment {
+
 
     public Frag1() {
         // Required empty public constructor
@@ -24,5 +26,7 @@ public class Frag1 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_1, container, false);
+
+
     }
 }
